d0fa48fb2a721e8927e7d4233f9afa3b96d1f365
Rahul Khanal <rahul39khanal@gmail.com>
2023-10-05 21:57:41 +0545

    class validation in nestjs

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Post, Req } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { Request } from "express";
+import { AuthDto } from "./dto";
+
+@Controller("auth")
+export class AuthController {
+    constructor(private authService: AuthService) {
+        // this.authService.test()
+    }
+
+    @Post("signup")
+    // signup(@Req() req: Request) {
+    // signup(@Body('email', ParseIntPipe) email: string) {
+    signup(@Body() dto: AuthDto) {
+        return this.authService.signup()
+    }
+
+    @Get("login")
+    login() {
+        return this.authService.login()
+    }
+}
